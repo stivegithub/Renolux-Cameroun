49bed74701525ddb8506ac1c05c14eadbd744cbe
stive <fossistive627@gmail.com>
2023-12-11 12:25:56 +0100

    second

--- src/components/Localisation.tsx
+++ src/components/Localisation.tsx
@@ -1,4 +1,3 @@
-// Localisation.tsx
 import React from 'react';
 import { FaMapMarkerAlt, FaPhone, FaEnvelope, FaFacebook, FaWhatsapp, FaInstagram } from 'react-icons/fa';
 
@@ -22,7 +21,16 @@ const Localisation: React.FC = () => {
         <p>renolux3@gmail.com</p>
       </div>
 
-      {/* Ajout des icônes et liens vers les réseaux sociaux */}
+      <div className="mb-4">
+        <iframe
+          title="Localisation"
+          className="w-full h-48 rounded-md"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d158859.0344236147!2d11.49039546434694!3d3.8480311805636867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10610b148868c2f5%3A0xc785d69f57bea051!2sYaound%C3%A9!5e0!3m2!1sen!2scm!4v1646094949387!5m2!1sen!2scm"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </div>
+
       <div className="flex items-center">
         <a href="https://www.facebook.com/profile.php?id=100084170042632&mibextid=hrBMPu" target="_blank" rel="noopener noreferrer" className="text-2xl mr-4 text-blue-600">
           <FaFacebook />
@@ -30,8 +38,7 @@ const Localisation: React.FC = () => {
         <a href="https://wa.me/message/WC73X73ZRMSHL1" target="_blank" rel="noopener noreferrer" className="text-2xl mr-4 text-green-600">
           <FaWhatsapp />
         </a>
-        {/* Ajoutez d'autres liens vers les réseaux sociaux au besoin */}
-        <a href="https://instagram.com/renolux_cameroun ?igshid=OGQ5ZDc2ODk2ZA==" target="_blank" rel="noopener noreferrer" className="text-2xl mr-4 text-pink-600">
+        <a href="https://instagram.com/renolux_cameroun?igshid=OGQ5ZDc2ODk2ZA==" target="_blank" rel="noopener noreferrer" className="text-2xl mr-4 text-pink-600">
           <FaInstagram />
         </a>
       </div>
